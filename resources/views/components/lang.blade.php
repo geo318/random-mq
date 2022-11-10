4e498a80622c12cee6db2c5b03d474d03373ff8a
geo318 <geo.lomidze@gmail.com>
2022-11-10 11:04:04 +0400

    style: index and movies pages

---
+++ resources/views/components/lang.blade.php
@@ -0,0 +1,5 @@
+<div {{ $attributes(['class' => "flex items-center justify-center w-[3.625rem] h-[3.625rem] rounded-full border-2 border-white text-[24px]"]) }}>
+    <a href="#">
+        {{ $slot }}
+    </a>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <main class="flex justify-center items-center h-screen flex-col">
+        <div class="max-w-[700px] rounded-[10px] overflow-hidden">
+            <img class="w-full" src="https://img.lovepik.com/photo/50085/5373.jpg_wh860.jpg" alt="">
+        </div>
+        <x-title class="mt-[65px] mb-[114px]">favourite excerpt</x-title>
+        <x-title class="underline">film title</x-title>
+    </main>
+</x-layout>
