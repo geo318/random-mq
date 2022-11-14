d11aa2afc95ba723f019ecec5b2346aee27ef780
geo318 <geo.lomidze@gmail.com>
2022-11-14 22:28:36 +0400

    fix: remove unused comment

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -7,11 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreUserRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
