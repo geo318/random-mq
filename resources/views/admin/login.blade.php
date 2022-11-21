379c235557062e2c0db350ade87a0f44d57eac14
geo318 <geo.lomidze@gmail.com>
2022-11-21 11:48:07 +0400

    fix: route redirection issues

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,20 +8,20 @@
             @csrf
             <input type="hidden" name="remember" value="true">
             <div class="space-y-px rounded-md shadow-sm">
-                <div class="mb-5">
-                    <label for="email" class="sr-only">{{ __('Email address') }}</label>
-                    <input id="email" name="email" type="email" value="{{ old('email') }}" autocomplete="email" required class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="{{ __('Email address') }}">
-                    @error('email')
-                        <div class="text-red-400 text-sm mt-1">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div>
-                    <label for="password" class="sr-only">Password</label>
-                    <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="{{ __('Password') }}">
-                    @error('password')
-                        <div class="text-red-400 text-sm mt-1">{{ $message }}</div>
-                    @enderror
-                </div>
+              <div class="mb-5">
+                <label for="email" class="sr-only">{{ __('Email address') }}</label>
+                <input id="email" name="email" type="email" value="{{ old('email') }}" autocomplete="email" required class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="{{ __('Email address') }}">
+                @error('email')
+                  <div class="text-red-400 text-sm mt-1">{{ $message }}</div>
+                @enderror
+              </div>
+              <div>
+                <label for="password" class="sr-only">Password</label>
+                <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="{{ __('Password') }}">
+                @error('password')
+                  <div class="text-red-400 text-sm mt-1">{{ $message }}</div>
+                @enderror
+              </div>
             </div>      
             <div>
               <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,12 +8,15 @@
     <title>Movie quotes app</title>
     @vite('resources/css/app.css')
   </head>
+
   <body class="font-['Sansation'] bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#4E4E4E] via-[#3D3B3B] to-[#3D3B3B] h-full w-full min-h-screen text-white">
         <header class="flex fixed inset-0 h-20 z-50">
+          {{ Session::get('applocale') }}
+          {{ app()->getLocale() }}
           <div class="grow p-5">
           @unless (request()->routeIs('home'))
             <div class="flex">
-              <a href="/" class="text-white mr-5"><span class="text-xl">&#8249; </span>{{ __('home') }}</a>
+              <a href="/{{ Session::get('applocale') ?? app()->getLocale() }}" class="text-white mr-5"><span class="text-xl">&#8249; </span>{{ __('home') }}</a>
               <a href="javascript:history.back()" class="text-white"><span class="text-xl"> </span>{{ __('back') }}</a>
             </div>
             
