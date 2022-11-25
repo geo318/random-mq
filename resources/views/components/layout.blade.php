af913b784174943e63e02e5c29c613613d045a53
geo318 <geo.lomidze@gmail.com>
2022-11-25 11:47:37 +0400

    feat: add favicons

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}?v=2">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}?v=2">
+    <link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}?v=2">
+    <link rel="mask-icon" href="{{ asset('favicons/safari-pinned-tab.svg') }}?v=2" color="#5bbad5">
     <link rel="stylesheet" href="{{ asset('fonts/font.css') }}">
     <title>Movie quotes app</title>
     @vite('resources/css/app.css')
